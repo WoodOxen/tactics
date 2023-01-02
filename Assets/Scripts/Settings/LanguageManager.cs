b56492b20c87af9004ebb007bfe6c03005392373
MotacillaAlba <rowena.rowena.lily@gmail.com>
2023-01-02 11:27:35 +0800

    stash

---
+++ Assets/Scripts/Settings/LanguageManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// This class manages scene switch in TACTICS
+/// </summary>
+public class LanguageManager : MonoBehavior 
+{
+
+    public enum LanguageType {
+        cn,
+        en,
+    }
+
+    public static LanguageType type = LanguageType.en;
+
+    public static void Init() {
+        if
+
+    }
+}
---
+++ README.zh-cn.md
@@ -0,0 +1,23 @@
+Language : [English](./README.md) | 中文
+
+# TACTICS
+
+## 关于
+
+Tactics
+
+## 开发
+
+### 版本
+
+当前版本：v0.0.0
+
+引擎版本：2021.3.6f1c1 (Unity)
+
+### 文档
+
+采用Doxygen生成
+
+[代码风格](https://learn.microsoft.com/zh-cn/dotnet/csharp/fundamentals/coding-style/coding-conventions)
+
+[文档首页 (需本地生成)](./Docs/html/index.html)
